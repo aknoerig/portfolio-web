74e19e38e8bc60cc169e88e291c8ed122dae6ab9
André Knörig <andre.knoerig@ixds.de>
2014-12-05 19:32:03 +0100

    tag view working

--- routes/views/tag.js
+++ routes/views/tag.js
@@ -6,34 +6,75 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 	
-	// Init locals
-	locals.section = 'tags';
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'works';
+	
 	locals.filters = {
 		tag: req.params.tag
 	};
 	locals.data = {
 		projects: [],
-		tags: []
+		industries: [],
+		technologies: [],
+		interactions: [],
 	};
-	
+
+
 	// Load all tags
+
 	view.on('init', function(next) {
 		
 		keystone.list('Industry').model.find().sort('name').exec(function(err, results) {
+			
 			if (err || !results.length) {
 				return next(err);
 			}
-			locals.data.tags = results;
+			
+			locals.data.industries = results;
+
+			next(err);
 		});
-		
+
 	});
-	
+
+	view.on('init', function(next) {
+
+		keystone.list('Technology').model.find().sort('name').exec(function(err, results) {
+			
+			if (err || !results.length) {
+				return next(err);
+			}
+			
+			locals.data.technologies = results;
+		
+			next(err);
+		});
+
+	});
+
+	view.on('init', function(next) {
+
+		keystone.list('Interaction').model.find().sort('name').exec(function(err, results) {
+			
+			if (err || !results.length) {
+				return next(err);
+			}
+			
+			locals.data.interactions = results;
+		
+			next(err);
+		});
+
+	});
+
 	// Load the current tag filter
 	view.on('init', function(next) {
 		
-		if (req.params.tag) {
+		if (req.params.tag && !locals.data.tag) {
 			keystone.list('Industry').model.findOne({ key: locals.filters.tag }).exec(function(err, result) {
 				locals.data.tag = result;
+				locals.data.property = 'industries';
 				next(err);
 			});
 		} else {
@@ -41,21 +82,50 @@ exports = module.exports = function(req, res) {
 		}
 		
 	});
-	
+
+	view.on('init', function(next) {
+		
+		if (req.params.tag && !locals.data.tag) {
+			keystone.list('Technology').model.findOne({ key: locals.filters.tag }).exec(function(err, result) {
+				locals.data.tag = result;
+				locals.data.property = 'technologies';
+				next(err);
+			});
+		} else {
+			next();
+		}
+		
+	});
+
+	view.on('init', function(next) {
+		
+		if (req.params.tag && !locals.data.tag) {
+			keystone.list('Interaction').model.findOne({ key: locals.filters.tag }).exec(function(err, result) {
+				locals.data.tag = result;
+				locals.data.property = 'interactions';
+				next(err);
+			});
+		} else {
+			next();
+		}
+		
+	});
+
+
 	// Load the Projects
 	view.on('init', function(next) {
 		
 		var q = keystone.list('Project').paginate({
 				page: req.query.page || 1,
-				perPage: 10,
+				perPage: 12,
 				maxPages: 10
 			})
 			.where('state', 'published')
 			.sort('-toDate')
-			.populate('industries');
+			.populate('industries technologies interactions');
 		
 		if (locals.data.tag) {
-			q.where('industries').in([locals.data.tag]);
+			q.where(locals.data.property).in([locals.data.tag]);
 		}
 		
 		q.exec(function(err, results) {
@@ -64,7 +134,8 @@ exports = module.exports = function(req, res) {
 		});
 		
 	});
-	
+
+
 	// Render the view
 	view.render('tag');
 	
